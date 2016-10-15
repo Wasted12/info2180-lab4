e1f334e1da7ab558d614cdce18257239296cba6f
Wasted12 <jordanphillipk@gmail.com>
2016-10-14 21:43:48 -0500

    Add files via upload
    Exercise 2

--- maze.js
+++ maze.js
@@ -1,7 +1,20 @@
+var x = $("div#maze div.boundary");
+
 window.onload = function(){
-	document.getElementById("boundary1").onmouseover = losefunction;
+	//document.getElementById("boundary1").onmouseover = losefunction;
+	for (var i=0; i < x.length; i++){
+		x[i].onmouseover = indfunction;
+	}
 }
 
-function losefunction(){
+
+/*function losefunction(){
 	document.getElementById("boundary1").className = "boundary youlose";
+}*/
+
+function indfunction(){
+	//document.querySelectorAll(".boundary").className = "boundary youlose";
+	for (var i=0; i < x.length; i++){
+		x[i].className = "boundary youlose";
+	}
 }
